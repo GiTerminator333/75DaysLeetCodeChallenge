1818741d473ea3395b476ef24bc4bc7ba372c751
Sushant <03sushant03@gmail.com>
2026-06-01 21:06:39 +0530

    Time: 7 ms (38.22%), Space: 32.7 MB (39.76%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countSubUpto(vector<int>& nums, int val){
+        //finding all valid subarrays with sum <= val
+        int l=0, r=0;
+        int count = 0;
+        int sum = 0;
+        while(r < nums.size()){
+            sum += nums[r];
+            while(l <= r && sum > val){
+                sum -= nums[l];
+                l++;
+            }
+            
+            if(sum <= val ) count += (r-l+1); //count all possible subarrays <= val
+            r++;
+        }
+
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return countSubUpto(nums,goal) - countSubUpto(nums,goal - 1);
+        //normal sliding window not possible here as adding 0 doesnt change the subarray sum
+        //this leads to extra subarray patterns which are missed in normal aliding window
+        //dry run testcase 1
+        //hence we will find
+        //subarray sum k = (all subarray with sum <= k) - (all subarray with sum <= k - 1)
+    }
+};
