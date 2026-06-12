14f8785657cad480bdc50fe8e85a85d4c96ed0ac
Sushant <03sushant03@gmail.com>
2026-06-12 23:53:47 +0530

    Time: 69 ms (84.84%), Space: 36.7 MB (78.26%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -2,40 +2,44 @@ typedef pair<int, pair<int, int>> pi;
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<pi> maxh;
-        priority_queue<pi, vector<pi>, greater<pi>> minh;
+        //Approach
+        //start from col = 0...group the ele -> find maxi and mini -> range = [mini, maxi]
+        //since the array is sorted...the only way to find the shorter range than current one is to find a minimum element from the mini 's row who's val > mini
+        //so u need to add element next to mini (since arrays are sorted)
+        //push it in the heap to track mini for each range
+        //also update maxi each time u push next ele (as it may be > maxi)
 
+        priority_queue<pi, vector<pi>, greater<pi>> minh;   //to track mini for each range
+        // {nums[row][col], {row, col}}
+
+        int maxi = INT_MIN;
         for(int i=0; i<nums.size(); i++){
+            maxi = max(maxi, nums[i][0]);
             minh.push({nums[i][0], {i, 0}});
-            maxh.push({nums[i][0], {i, 0}});
         }
 
-        int minIdx = -1, maxIdx = -1;
-        int minDiff = INT_MAX;
-        vector<int> ans;
-
+        int start = -1e6, end = 1e6; //to track the shortest range
         while(!minh.empty()){
-            pi mini = minh.top();
-            pi maxi = maxh.top();
-
+            pi hTop = minh.top();
             minh.pop();
 
-            int lowerLimit = mini.first, upperLimit = maxi.first;
-            if(minDiff > upperLimit - lowerLimit){
-                minDiff = upperLimit - lowerLimit;
-                ans = {lowerLimit, upperLimit};
+            int row = hTop.second.first;
+            int col = hTop.second.second;
+            int mini = hTop.first;
+
+            if(end - start > maxi - mini){
+                end = maxi;
+                start = mini;
             }
-
-            int row = mini.second.first;
-            int col = mini.second.second;
-
+            
             if(col < nums[row].size() - 1){
                 minh.push({nums[row][col + 1], {row, col + 1}});
-                maxh.push({nums[row][col + 1], {row, col + 1}});
+                maxi = max(maxi, nums[row][col + 1]);
             }
             else break;
+
         }
 
-        return ans;
+        return {start, end};
     }
 };
