c1f0fbb7af27229c3d546b4fdbaaa498462feaf1
Sushant <03sushant03@gmail.com>
2026-06-30 15:34:25 +0530

    Time: 0 ms (100%), Space: 7.7 MB (66.57%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    int add(int carry, int sum){
+        if(!carry) return sum;
+        int nCarry = carry & sum;
+        int nSum = carry ^ sum;
+
+        return add(nCarry<<1, nSum);
+    }
     int getSum(int a, int b) {
-        return (a|b) + (a&b);
+        return add(a, b);
+
+        //basically for 1 bit xor means addition
+        // but 1^1 = 0
+        //so for real addition we & the same bits so that we know which bit addion produce carry
+        //then we left shift those carry bits just like real multi bit addition and 
     }
 };
