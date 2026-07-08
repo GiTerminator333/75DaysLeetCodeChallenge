c6451dfe8c6d46992318db41cd1d97a9d1919de8
Sushant <03sushant03@gmail.com>
2026-07-08 18:24:14 +0530

    Time: 0 ms (100%), Space: 9.3 MB (19.48%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(vector<int> &nums, int target){ 
-        if(target == 0){
-            return dp[0];
-        }
+    // vector<int> dp;
+    // int solve(vector<int> &nums, int target){ 
+    //     if(target == 0){
+    //         return dp[0];
+    //     }
 
-        if(dp[target] != -1) return dp[target];
+    //     if(dp[target] != -1) return dp[target];
 
-        int ans = 0;
-        for(int i = 0; i<nums.size(); i++){
-            if(target < nums[i]) continue;
-            ans += solve(nums, target - nums[i]);
-        }
+    //     int ans = 0;
+    //     for(int i = 0; i<nums.size(); i++){
+    //         if(target < nums[i]) continue;
+    //         ans += solve(nums, target - nums[i]);
+    //     }
 
-        return dp[target] = ans;
-    }
+    //     return dp[target] = ans;
+    // }
     int combinationSum4(vector<int>& nums, int target) {
-        dp.resize(target + 1, -1);
+        // dp.resize(target + 1, -1);
+        // dp[0] = 1;
+        // return solve(nums, target);
+
+        vector<unsigned int> dp(target + 1);
         dp[0] = 1;
-        return solve(nums, target);
+
+        for(int i=1; i<dp.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                if(nums[j] > i) continue;
+
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+
+        return dp[target];
     }
 };
