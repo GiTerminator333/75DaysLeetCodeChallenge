16f0e4e7168e61509f782d05d3d98afea333f73b
Sushant <03sushant03@gmail.com>
2026-07-15 21:04:29 +0530

    Time: 0 ms (100%), Space: 8 MB (66.99%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    int mini = INT_MAX;
-    string res;
-    void helper(string &pattern, string &s, vector<bool> &visited, int idx){
-        if(idx < 0){
-            int val = stoi(s);
-            if(mini > val){
-                mini = val;
-                res = s;
-            }
-            return;
-        }
-
-        for(int i=0; i<9; i++){
-            if(!visited[i]){
-                char c = '1' + i;
-                if((pattern[idx] == 'I' && s[0] < c) || (pattern[idx] == 'D' && s[0] > c)) continue;
-                visited[i] = true;
-                s.insert(s.begin(), c);
-                helper(pattern, s, visited, idx - 1);
-                s.erase(s.begin());
-                visited[i] = false;
-            }
-        }
-    } 
     string smallestNumber(string pattern) {
-        vector<bool> visited (9, false);
+        char count = '1';
         string s;
-
-        int n = pattern.length();
-
-        for(int i = 0; i<9; i++){
-            visited[i] = true;
-            s.push_back('1' + i);
-            helper(pattern, s, visited, n - 1);
-            s.pop_back();
-            visited[i] = false;
+        stack<char> st;
+        // st.push(count);
+        for(int i=0; i<pattern.size(); i++){
+            st.push(count);
+            if(pattern[i] == 'I'){
+                while(!st.empty()){
+                    s.push_back(st.top());
+                    st.pop();
+                }
+            }
+            count++;
         }
 
-        return res;
+        st.push(count); //last number
+
+        while(st.size()){
+            s.push_back(st.top());
+            st.pop();
+        }
+
+        return s;
     }
 };
